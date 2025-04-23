187bd8121027d05200df95437d5fb43aca911edc
808arc <808arc>
2025-04-22 22:35:10 -0400

    now files are stored in archive dir

--- log_archive.sh
+++ log_archive.sh
@@ -4,23 +4,25 @@ find_log(){
     path=$1
     find "$path" -type d
 
-    if ! true; then
+    if [ ! -d "$path"]; then
         echo "Error: $path does not exist"
         exit
     else
         echo "$path exists"
     fi
 }
-
 archive(){
     path=$1
     dir_name="${path##*/}"
     timestamp=$(date +"%Y%m%d_%H%M%S")
-    
+    archive_dir="archive"
 
-    tar -cf "${dir_name}_archive_${timestamp}.tar.gz" "$path"
+    mkdir -p "$archive_dir"
+    tar -czf "${arichve_dir}/${dir_name}_archive_${timestamp}.tar.gz" "$path"
 }
-
 
 find_log "$1"
 archive "$1"
+
+#Archive directory not created or used:
+# No logging of archive date/time: ???
